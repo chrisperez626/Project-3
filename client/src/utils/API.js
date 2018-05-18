45aae3f80f70c2df5ee9eb169b31de25d6e5eabe
kavitha <kavitha.veluchamy@gmail.com>
2018-05-18 10:07:12 -0400

    added client and server side routes and controllers

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -15,5 +15,35 @@ export default {
 
   logout: () => {
     return axios.get("/auth/logout");
+  },
+  getProject: function(){
+    return axios.get("/api/projects");
+  },
+  saveProject: function(projectData){
+    return axios.post("/api/projects",projectData);
+  },
+  getProject: function(id){
+    return axios.get("/api/projects/"+id);
+  },
+  updateProject: function(id, projectData){
+    return axios.post("/api/projects/"+id, projectData);
+  },
+  deleteProject: function(id){
+    return axios.delete("/api/projects/"+id);
+  },
+  getTask: function(){
+    return axios.get("/api/tasks");
+  },
+  saveTask: function(taskData){
+    return axios.post("/api/tasks",taskdata);
+  },
+  getTask: function(id){
+    return axios.get("/api/tasks/"+id);
+  },
+  updateTask: function(id,taskData){
+    return axios.post("/api/tasks/"+id, tasktData);
+  },
+  deleteTask: function(id){
+    return axios.delete("/api/tasks/"+id);
   }
 }
--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,6 @@
 module.exports = {
-    auth: require("./auth")
-  }
+  auth: require("./auth"),
+  projects: require("./projectsController"),
+  tasks:require("./taskController")
+}
   
