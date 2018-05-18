492e98dc9e4b78b1c23ddf8ecaf4e0b7424a22c3
Joe Christopher <joechristopher@icloud.com>
2018-05-18 10:30:16 -0400

    added deployment details

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Whiteboard from "./components/Whiteboard/Whiteboard";
+
+
+const App = () => (
+  <div className= "siteLayout">
+    <Whiteboard />
+  </div>
+);
+
+
+
+export default App;
