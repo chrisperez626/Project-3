eb0f11a22cda46da0f11b89497d5329cedcc3519
chrisperez626 <chrisperez626@gmail.com>
2018-05-22 10:23:49 -0400

    fixed task divs

--- client/src/components/TaskGroup/Task-Group.js
+++ client/src/components/TaskGroup/Task-Group.js
@@ -13,39 +13,42 @@ class TaskGroup extends Component {
         taskName: ""
     }
 
-    componentDidMount = () =>{
+    loadTasks = () =>{
         API.getAllTask()
-        .then(res =>{
-            this.setState({divCount: res.data})
-        }).catch(err =>{
+        .then(res => {
+            this.setState({ divCount: res.data })
+        }).catch(err => {
             console.log(err)
         })
     }
 
-    newTask = () =>{
-        this.setState({NewTask:true})
+    componentDidMount = () => {
+        this.loadTasks()
+    }
+
+    newTask = () => {
+        this.setState({ NewTask: !this.state.NewTask })
         console.log(this)
-        
+
     }
 
-    submitTask = event =>{
-        event.preventDefault()
-        this.setState({NewTask:false})
-        API.saveTask({taskname: this.state.taskName, status: this.props.header, UserId: "1", ProjectId:'1'}).then(function(data, err){
-            if (err) throw err;
+    submitTask = event => {
+        // event.preventDefault()
+        this.setState({ NewTask: !this.state.NewTask })
+        API.saveTask({ taskname: this.state.taskName, status: this.props.header, UserId: "1", ProjectId: '1' })
+        .then(data=>{
+            this.loadTasks()
         })
-        API.getAllTask()
-        .then(res =>{
-            this.setState({divCount: res.data})
-        }).catch(err =>{
+        .catch(err=>{
             console.log(err)
         })
+        
 
     }
 
     handleInputChange = event => {
-        const {name, value} = event.target;
-        this.setState({taskName: value});
+        const { name, value } = event.target;
+        this.setState({ taskName: value });
 
     }
 
@@ -53,38 +56,38 @@ class TaskGroup extends Component {
         return (
             <div>
                 <div id={this.props.id} className='card'>
-                    <header id={this.props.id}className=' card-header text-center'>{this.props.header}</header>
+                    <header id={this.props.id} className=' card-header text-center'>{this.props.header}</header>
                     <div className='card-body'>
                         {!this.state.NewTask ?
-                    (   
-                        <div>
-                            <div>
-                                <button onClick={() => this.newTask()}>Add new task +</button>
-                            </div>
-                            {this.state.divCount.map(task=>{
-                                if(this.props.header === task.status)
-                                return (
-                                    <Task
-                                    key={task.id}
-                                    id={task.id}
-                                    content={task.taskname}
-                                    status={task.status}/>
-                                )
-                            })}
-                        </div>
-                        )
-                    :
-                    (
-                        <div>
-                            <form onSubmit={this.submitTask}>
-                                <textarea id='newTask' placeholder='Add new task here' onChange={this.handleInputChange} ></textarea>
-                                <br />
-                                <button type='submit'>Add Task</button>
-                                <button onClick={this.newTask}>Cancel</button>
-                            </form>
-                        </div>
-                        )
-                    }
+                            (
+                                <div>
+                                    <div>
+                                        <button onClick={() => this.newTask()}>Add new task +</button>
+                                    </div>
+                                    {this.state.divCount.map(task => {
+                                        if (this.props.header === task.status)
+                                            return (
+                                                <Task
+                                                    key={task.id}
+                                                    id={task.id}
+                                                    content={task.taskname}
+                                                    status={task.status} />
+                                            )
+                                    })}
+                                </div>
+                            )
+                            :
+                            (
+                                <div>
+                                    <form onSubmit={this.submitTask}>
+                                        <textarea id='newTask' placeholder='Add new task here' onChange={this.handleInputChange} ></textarea>
+                                        <br />
+                                        <button type='submit'>Add Task</button>
+                                        <button onClick={this.newTask}>Cancel</button>
+                                    </form>
+                                </div>
+                            )
+                        }
                     </div>
                 </div>
             </div>
